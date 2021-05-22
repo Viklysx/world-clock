8c1b9867c1b431410bfe03772fc39180e3eab8b6
Viklysx <vikbelec111@yandex.ru>
2021-05-23 01:15:16 +0300

    feat: add function in component

--- clocks/src/components/WorldClock.js
+++ clocks/src/components/WorldClock.js
@@ -1,31 +1,60 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
+import WorldClockModel from './WorldClockModel';
 
 class WorldClock extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             time: new Date(),
-            name: '',
+            title: '',
             hour: '',
             clocks: []
         }
     }
 
+    handleDataChange = (evt) => {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    handleSubmit = (evt) => {
+        evt.preventDefault(); 
+        const clocks = new WorldClockModel(nanoid(), this.state.title, this.state.hour);
+        this.setState({
+            clocks: [...this.state.clocks, clocks]
+        }) 
+    }
+
     componentDidMount() {
-        
+        this.time = setInterval(() => {
+            this.setState({
+                time: new Date(Date.now() + this.state.hour*60*60*1000).toLocaleTimeString()
+            })
+        }, 1000)
     }
 
     componentWillUnmount() {
-
+        clearInterval(this.time);
     }
 
     render() {
         return (
             <>
-              <form>
-                <input type="text" name="name" placeholder="Название"/>
-                <input type="number" name="timezone" placeholder="Временная зона"/>
+              <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text" 
+                    name="title" 
+                    value={this.state.title} 
+                    onChange={this.handleDataChange} 
+                    placeholder="Название"/>
+                <input 
+                    type="number" 
+                    name="hour" 
+                    value={this.state.hour} 
+                    onChange={this.handleDataChange} 
+                    placeholder="Временная зона"/>
                 <button type="submit">Добавить</button>
               </form>     
               <div>
---
+++ clocks/src/components/WorldClockModel.js
@@ -0,0 +1,9 @@
+class WorldClockModel {
+    constructor(id, title, hour) {
+        this.id = id;
+        this.title = title;
+        this.hour = hour;
+    }
+}
+
+export default WorldClockModel;
